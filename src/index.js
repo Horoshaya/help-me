175dee375d68a7a77e53622342b779d0e14d915c
Horoshaya <43165019+Horoshaya@users.noreply.github.com>
2018-10-03 16:50:41 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,44 +1,35 @@
 module.exports = function count(s, pairs) {
   let str = s.split('');
-  let N = 1;
-  let simple = [];
-  if (str.length > 10) return false;
-  for (let i = 0; pairs.length > i; i++) {
-    if (pairs[i][1] > 10) return false;
-    let a = pairs[i][0];
-    let b = pairs[i][1];
-    simple[i] = pairs[i][0];
-    N = N * Math.pow(a, b);
-  }
-//Заглушка
-  if (N > 100000000) return false;
+	let N = 1;
+	let simple = [];
+	let count = 0;
 
-  let simpleLength = simple.length;
-  let allNumbers = '';
+	if (str.length > 10) return false; // Заглушка
 
-  for (let i = 0; N > i; i++) {
-    let ifNotDivides = 0;
-    
-    for (let j = 0; simpleLength > j; j++) {
-      //console.log(i, i % simple[i]);
-      if (i % simple[j] != 0) {
-        ifNotDivides++;
-      } 
-    }
-    if (ifNotDivides == simpleLength) {
-      allNumbers += '1';
-    } else {
-      allNumbers += '0';
-    }
-  }
+	for (let i = 0; pairs.length > i; i++) {
+		if (pairs[i][1] > 10) return false; // Заглушка
+		simple[i] = pairs[i][0];
+		N = N * Math.pow(pairs[i][0], pairs[i][1]);
+	}
+	//Заглушка
+	if (N > 10000000000) return false;
 
-  let count = 0;
-  for (let i = 0; N > i; i++) {
-      let v = allNumbers.indexOf(s);
-      if (allNumbers.length >= s.length && v >= 0) {
-        allNumbers = allNumbers.slice(v+str.length);
-        count++;
-      } else break;     
-  }
-  return count;
+	if (s.length == 1) {
+		count =  pairs.reduce((k, current) => k * (current[0] - 1), 1);		
+
+		(s == '0') ? count = N - count : count;
+		return count % 1000000007;
+	}
+
+	let allNumbers = '0';
+	for (let i = 1; i <= N; i++) {
+		(simple.some(j => i % j == 0)) ? allNumbers += '0': allNumbers += '1';
+		
+		let index = allNumbers.indexOf(s);
+		if (allNumbers.length >= s.length && index >= 0) {
+			allNumbers = allNumbers.slice(index + str.length - 1);
+			count++;
+		}
+	}
+	return count;
 }
